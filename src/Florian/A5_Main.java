f766e8a776eceae4fb39ff8c35cf7a151b18fc38
florian <flo.moerzinger@gmx.at>
2020-07-13 16:36:42 +0200

    second

--- src/Florian/A5_Main.java
+++ src/Florian/A5_Main.java
@@ -1,8 +1,30 @@
 package Florian;
 
+import java.util.ArrayList;
+
 public class A5_Main {
 
     public static void main(String[] args) {
+
+    ArrayList<A5_Warehouse> d;
+
+
+      A5_Warehouse thing = new A5_Warehouse();
+        //thing.fillIt();
+
+    d = thing.getInventory();
+
+    //System.out.print(d.size());
+
+        for(int i = 0; i < d.size(); i++){
+
+            d.get(i).formatted();
+
+        }
+
+
+
+
 
     }
 }
--- src/Florian/A5_Warehouse.java
+++ src/Florian/A5_Warehouse.java
@@ -1,7 +1,50 @@
 package Florian;
 
+import java.util.ArrayList;
+
 public class A5_Warehouse {
 
+    private String article;
+    private int amount;
 
 
+
+    A5_Warehouse(String article, int amount){
+        this.article = article;
+        this.amount = amount;
+
+    }
+    A5_Warehouse(){
+
+    }
+
+        public void formatted(){
+
+        System.out.println("Article: " + article + " Amount: " + amount );
+
+        }
+        private ArrayList<A5_Warehouse> inventory = new ArrayList<A5_Warehouse>();
+
+
+        private void fillIt(){
+            A5_Warehouse thing1 = new A5_Warehouse("basket", 30);
+            A5_Warehouse thing2 = new A5_Warehouse("ball", 59);
+            A5_Warehouse thing3 = new A5_Warehouse("bird", 8);
+
+
+            inventory.add(thing1);
+            inventory.add(thing2);
+            inventory.add(thing3);
+
+
+        }
+
+
+    public ArrayList<A5_Warehouse> getInventory() {
+        fillIt();
+        return new ArrayList<A5_Warehouse>(inventory);
+    }
 }
+
+
+
